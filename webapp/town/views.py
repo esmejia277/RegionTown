27d1c71904e1fa4ea7673e0f4c807795b690eb86
Esteban Mejia <estebanmejia277@gmail.com>
2019-08-06 16:36:31 -0500

    Message unique code

--- webapp/town/views.py
+++ webapp/town/views.py
@@ -4,7 +4,6 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse
 from django.contrib import messages 
-from django.contrib.messages.views import SuccessMessageMixin 
 from django import forms
 
 class ListTown(ListView):
